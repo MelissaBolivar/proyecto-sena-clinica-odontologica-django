772241b29f1b62636b60426c926c2e44f125215a
nicolascorderodev <nicolascordero281@gmail.com>
2025-01-25 18:35:06 -0500

    Proyecto Final Sena desde 0

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('servicios/', views.servicios, name='servicios'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('login/', views.login_view, name = 'login'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from.forms import Contactanos
+
+#Vistas de usuario.
+def home(request):
+    return render(request, 'index.html')
+
+def servicios(request):
+    return render(request, 'servicios.html')
+
+def contacto(request):
+    if request.method == 'POST':
+        form = Contactanos(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.')
+            return redirect('contacto')
+    else:
+        form = Contactanos()
+    
+    return render(request, 'contactanos.html', {'form': form})
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def auxadmin_view(request):
+    return render(request, 'auxadmin.html')
+
+def dentista_view(request):
+    return render(request, 'dentista.html')
+
+def paciente_view(request):
+    return render(request, 'paciente.html')
